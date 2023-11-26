7ce2196a94626612e4bedb57e868c9b8815c0420
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-11-26 17:04:49 -0300

    refactor: ajusta cor fundo patrocinadores

--- src/components/organisms/Supporters/index.tsx
+++ src/components/organisms/Supporters/index.tsx
@@ -24,26 +24,28 @@ export interface SupportersProps {
 
 function Supporters({title, subtitle, tabItems, sponsors, volunteers} : SupportersProps) {
     return (
-        <div id="supporters" className="container mx-auto py-12 px-0 md:py-14">
-            <div className="mb-8">
-                <Text size="secondary">{title}</Text>
-                <Text size="tertiary">{subtitle}</Text>
-            </div>
-            <Selector tabItems={tabItems} changeItem={() => {}}/>
-            <div className="flex justify-around items-center flex-wrap">
-                {sponsors.map((sponsor, index) => (
-                    <a key={index} href={sponsor.link} target="_blank">
-                        <img className="sponsors_image" src={sponsor.image as string} alt={sponsor.alt}/>
-                    </a>
-                ))}
-            </div>
-            <div className="flex justify-around items-center flex-wrap">
-                {volunteers.map((volunteer, index) => (
-                   <div key={index}>
-                        <img className="sponsors_image" src={volunteer.image as string} alt={volunteer.alt}/>
-                        <p>{volunteer.name}</p>
-                    </div>
-                ))}
+        <div id="supporters" className="w-full bg-white">
+            <div className="container mx-auto py-12 px-0 md:py-14 ">
+                <div className="mb-8">
+                    <Text size="secondary">{title}</Text>
+                    <Text size="tertiary">{subtitle}</Text>
+                </div>
+                <Selector tabItems={tabItems} changeItem={() => {}}/>
+                <div className="flex justify-around items-center flex-wrap">
+                    {sponsors.map((sponsor, index) => (
+                        <a key={index} href={sponsor.link} target="_blank">
+                            <img className="sponsors_image" src={sponsor.image as string} alt={sponsor.alt}/>
+                        </a>
+                    ))}
+                </div>
+                <div className="flex justify-around items-center flex-wrap">
+                    {volunteers.map((volunteer, index) => (
+                    <div key={index}>
+                            <img className="sponsors_image" src={volunteer.image as string} alt={volunteer.alt}/>
+                            <p>{volunteer.name}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
