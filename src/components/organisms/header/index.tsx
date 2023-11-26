bb30cdeb482c7a61f77d3cf006270bdaa5245365
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-11-26 12:33:29 -0300

    refactor: remove referencia SingProps do HeaderProps

--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -2,17 +2,18 @@ import { useState } from "react";
 
 import { ReactComponent as MenuIcon } from "../../../assets/icons/menu.svg";
 
-import Sign, { SingProps } from "../sign";
+import Sign from "../sign";
 import Logged from "../../molecules/Logged";
 import Logo from "../../molecules/logo";
 import { ItemMenu, SocialLink } from "../../../types/baseTemplate";
 import MainMenu from "../mainMenu";
 
 
-export interface HeaderProps extends SingProps {
+export interface HeaderProps {
     itemsMenu: ItemMenu[]
     homeLink?: string;
     socialLinks: SocialLink;
+    solid: boolean;
 }
 
 function Header({ itemsMenu, socialLinks, solid } : HeaderProps) {
