776d234b7458dda113ec4485f9d4d7ca45f9350b
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-11-26 17:29:33 -0300

    refactor: altera nome componente de BaseHeroTemplate para HeroTemplate

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,12 +3,12 @@ import ActionAreas from "../../components/organisms/actionAreas"
 import Features from "../../components/organisms/features"
 import Map from "../../components/organisms/map"
 import Supporters from "../../components/organisms/Supporters"
-import BaseHeroTemplate from "../../components/templates/heroTemplate"
+import HeroTemplate from "../../components/templates/heroTemplate"
 import { about_us, actionAreas, features, footer, header, hero, supporters } from "./data"
 
 function Home(){
     return (
-        <BaseHeroTemplate header={header} hero={hero} footer={footer}>
+        <HeroTemplate header={header} hero={hero} footer={footer}>
             <>
                 <AboutUs {...about_us} />
                 <Features {...features}/>
@@ -16,7 +16,7 @@ function Home(){
                 <Supporters {...supporters} />
                 <Map />
             </>
-        </BaseHeroTemplate>
+        </HeroTemplate>
     )
 }
 
