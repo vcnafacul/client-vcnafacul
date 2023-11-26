adf0e695ba45e97d5adf9f36768c41f3a83d0a56
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-11-26 12:32:09 -0300

    fix: corrige header scroll

--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { ReactComponent as MenuIcon } from "../../../assets/icons/menu.svg";
 
@@ -16,45 +16,27 @@ export interface HeaderProps extends SingProps {
 }
 
 function Header({ itemsMenu, socialLinks, solid } : HeaderProps) {
-    const [backgroundSolid, setBackgroundSolid] = useState(!!solid);
     const [openMenu, setOpenMenu] = useState(false);
-    console.log(`backgroundSolid ${backgroundSolid}`)
-
+    
     const token = ''
 
-    useEffect(() => {
-        if (solid !== true) {
-            const handleScroll = () => {
-                if (window.scrollY > 150 && !backgroundSolid) {
-                    setBackgroundSolid(true);
-                } else if (window.scrollY <= 150 && backgroundSolid) {
-                    setBackgroundSolid(false);
-                }
-            }
-
-            window.addEventListener("scroll", handleScroll);
-
-            return () => window.removeEventListener("scroll", handleScroll);
-        }
-    }, [backgroundSolid, solid]);
-
     return (
-        <header className="container mx-auto">
-            <div className={`fixed top-0 left-0 w-full z-50 py-4 md:py-5 px-0 ${backgroundSolid ? 'bg-white' : 'bg-transparent'} text-white`}>
+        <header className={`container mx-auto`}>
+            <div className={`fixed top-0 left-0 w-full z-50 py-4 md:py-5 px-0 text-white ${solid ? 'bg-white' : 'bg-transparent'}`}>
                 <div>
                     <div className="flex justify-between items-center mx-4 md:mx-auto md:max-w-6xl">
                         {openMenu ? <></> :
                             <div onClick={() => setOpenMenu(true) }>
-                                <MenuIcon className={`${!backgroundSolid ? 'fill-white' : 'fill-marine'}`} />
+                                <MenuIcon className={`${!solid ? 'fill-white' : 'fill-marine'}`} />
                             </div>
                         }
-                        <Logo solid={backgroundSolid} name />
-                        <MainMenu itemsMenu={itemsMenu} socialLinks={socialLinks} solid={backgroundSolid} open={openMenu} 
+                        <Logo solid={solid} name />
+                        <MainMenu itemsMenu={itemsMenu} socialLinks={socialLinks} solid={solid} open={openMenu} 
                         handleClose={() =>{
                             setOpenMenu(false)
                         }}/>
                         {!token ?  
-                            <Sign solid={backgroundSolid} className="items-center"/> :
+                            <Sign solid={solid} className="items-center"/> :
                             <Logged />
                         }
                     </div>
--- src/components/templates/baseTemplate/index.tsx
+++ src/components/templates/baseTemplate/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { LegacyRef, useEffect, useRef, useState } from "react";
 import Header, { HeaderProps } from "../../organisms/header"
 import Hero, { HeroProps } from "../../organisms/hero"
 import Footer, { FooterProps } from "../../organisms/footer";
@@ -11,9 +11,31 @@ export interface BaseTemplateProps{
 }
 
 function BaseTemplate({ header, hero, children, footer }: BaseTemplateProps){
+    const [solid, setSolid] = useState(false);
+    const scrollContainerRef = useRef<HTMLElement>(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (scrollContainerRef.current) {
+                const shouldBeSolid = scrollContainerRef.current.scrollTop > 150;
+                setSolid(shouldBeSolid)
+            }
+        };
+
+        const scrollContainer = scrollContainerRef.current;
+        if (scrollContainer) {
+            scrollContainer.addEventListener('scroll', handleScroll);
+        }
+
+        return () => {
+            if (scrollContainer) {
+            scrollContainer.removeEventListener('scroll', handleScroll);
+            }
+        };
+    }, []);
     return (
-        <div className="flex flex-col">
-            <Header {...header} />
+        <div ref={scrollContainerRef as LegacyRef<HTMLDivElement>} className="flex flex-col overflow-y-auto scrollbar-hide h-screen">
+            <Header itemsMenu={header.itemsMenu} socialLinks={header.socialLinks} solid={solid} />
             <Hero {...hero} />
             { children }
             <Footer {...footer} />
