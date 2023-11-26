ba2210aa5c899dd6de823a7ddd4df923b313ee66
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-11-26 17:27:54 -0300

    refactor: refatora logica de scroll para HeroTemplate

--- src/components/templates/baseHeroTemplate/index.tsx
+++ src/components/templates/baseHeroTemplate/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { LegacyRef, useEffect, useRef, useState } from "react";
 import { HeaderProps } from "../../organisms/header"
 import Hero, { HeroProps } from "../../organisms/hero"
 import  { FooterProps } from "../../organisms/footer";
@@ -13,9 +13,32 @@ export interface BaseHeroTemplateProps{
 
 function BaseHeroTemplate({ header, hero, children, footer }: BaseHeroTemplateProps){
     
+    const [solid, setSolid] = useState(false);
+    const scrollContainerRef = useRef<HTMLElement>(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (scrollContainerRef.current) {
+                const shouldBeSolid = scrollContainerRef.current.scrollTop > 150;
+                setSolid(shouldBeSolid)
+            }
+        };
+
+        const scrollContainer = scrollContainerRef.current;
+        if (scrollContainer) {
+            scrollContainer.addEventListener('scroll', handleScroll);
+        }
+
+        return () => {
+            if (scrollContainer) {
+            scrollContainer.removeEventListener('scroll', handleScroll);
+            }
+        };
+    }, []);
+
     return (
-        <div>
-            <BaseTemplate header={header} footer={footer}>
+        <div ref={scrollContainerRef as LegacyRef<HTMLDivElement>} className="flex flex-col overflow-y-auto scrollbar-hide h-screen">
+            <BaseTemplate header={header} footer={footer} solid={solid} >
                 <Hero {...hero} />
                 { children }
             </BaseTemplate>
--- src/components/templates/baseTemplate/index.tsx
+++ src/components/templates/baseTemplate/index.tsx
@@ -1,38 +1,19 @@
-import React, { LegacyRef, useEffect, useRef, useState } from "react";
+import React from "react";
 import Header, { HeaderProps } from "../../organisms/header"
 import Footer, { FooterProps } from "../../organisms/footer";
 
 export interface BaseTemplateProps{
     header: HeaderProps;
-    children: React.ReactNode
-    footer: FooterProps
+    children: React.ReactNode;
+    footer: FooterProps;
+    solid: boolean;
+    className?: string;
 }
 
-function BaseTemplate({ header, children, footer }: BaseTemplateProps){
-    const [solid, setSolid] = useState(false);
-    const scrollContainerRef = useRef<HTMLElement>(null);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (scrollContainerRef.current) {
-                const shouldBeSolid = scrollContainerRef.current.scrollTop > 150;
-                setSolid(shouldBeSolid)
-            }
-        };
-
-        const scrollContainer = scrollContainerRef.current;
-        if (scrollContainer) {
-            scrollContainer.addEventListener('scroll', handleScroll);
-        }
-
-        return () => {
-            if (scrollContainer) {
-            scrollContainer.removeEventListener('scroll', handleScroll);
-            }
-        };
-    }, []);
+function BaseTemplate({ header, children, footer, solid, className }: BaseTemplateProps){
+    
     return (
-        <div ref={scrollContainerRef as LegacyRef<HTMLDivElement>} className="flex flex-col overflow-y-auto scrollbar-hide h-screen">
+        <div className={className}>
             <Header itemsMenu={header.itemsMenu} socialLinks={header.socialLinks} solid={solid} />
             { children }
             <Footer {...footer} />
