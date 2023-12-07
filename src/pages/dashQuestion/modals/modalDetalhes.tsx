2e04e65b7f86d47c903204f0c79e3fb7ebd24fdf
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-12-07 15:20:39 -0300

    refactor: remove variavel desnecessaria

--- src/pages/dashQuestion/modals/modalDetalhes.tsx
+++ src/pages/dashQuestion/modals/modalDetalhes.tsx
@@ -12,7 +12,7 @@ import { AreaEnem, ArrayCor, Edicao } from "../data";
 import ModalImage from "../../../components/atoms/modalImage";
 import { BtnProps } from "../../../types/generic/btnProps";
 import { StatusEnum } from "../../../types/generic/statusEnum";
-import { ConverteQuestiontoUpdateQuestion, UpdateQuestion } from "../../../dtos/question/updateQuestion";
+import { UpdateQuestion } from "../../../dtos/question/updateQuestion";
 import { useForm } from 'react-hook-form';
 
 interface ModalDetalhesProps extends ModalProps {
@@ -25,7 +25,6 @@ interface ModalDetalhesProps extends ModalProps {
 function ModalDetalhes({ question, infos, handleClose, handleUpdateQuestionStatus, handleUpdateQuestion } : ModalDetalhesProps) {
     const { register, handleSubmit } = useForm();
     const [ isEditing, setIsEditing ] = useState<boolean>(false);
-    const [newState, setNewState] = useState<Question>(question)
     const [photoOpen, setPhotoOpen] = useState<boolean>(false);
 
 
@@ -40,33 +39,33 @@ function ModalDetalhes({ question, infos, handleClose, handleUpdateQuestionStatu
     frentes.push({ label: '', value: ''})
 
     const listFieldClassification : FormFieldInput[]= [
-        {id: "id", type: "text", label: "ID da questão", value: newState._id, disabled: true,},
-        {id: "exame",  type: "option", label: "Exame:*", options: exames, value: newState.exame._id, disabled: !isEditing,},
-        {id: "ano", type: "number", label: "Ano:*", value: newState.ano, disabled: !isEditing,},
-        {id: "edicao", type: "text", label: "Edição:*", options: Edicao, value: newState.edicao, disabled: !isEditing,},
-        {id: "cor", type: "option", label: "Cor do Caderno:*", options: ArrayCor, value: newState.caderno, disabled: !isEditing,},
-        {id: "numero", type: "number", label: "Número da Questão do Caderno:*", value: newState.numero, disabled: !isEditing,},
-        {id: "area", type: "option", label: "Área do Conhecimento:*", options: AreaEnem, value: newState.enemArea, disabled: !isEditing,},
-        {id: "materia", type: "option", label: "Disciplina:*", options: materias, value: newState.materia._id, disabled: !isEditing,},
-        {id: "frente1", type: "option", label: "Frente Principal:*", options: frentes, value: newState.frente1._id, disabled: !isEditing,},
-        {id: "frente2", type: "option", label: "Frente Secundária", options: frentes, value: newState.frente2 ? newState.frente2._id : '', disabled: !isEditing,},
-        {id: "frente3", type: "option", label: "Frente Terciária", options: frentes, value: newState.frente3 ? newState.frente3._id : '', disabled: !isEditing,},
+        {id: "id", type: "text", label: "ID da questão", value: question._id, disabled: true,},
+        {id: "exame",  type: "option", label: "Exame:*", options: exames, value: question.exame._id, disabled: !isEditing,},
+        {id: "ano", type: "number", label: "Ano:*", value: question.ano, disabled: !isEditing,},
+        {id: "edicao", type: "text", label: "Edição:*", options: Edicao, value: question.edicao, disabled: !isEditing,},
+        {id: "cor", type: "option", label: "Cor do Caderno:*", options: ArrayCor, value: question.caderno, disabled: !isEditing,},
+        {id: "numero", type: "number", label: "Número da Questão do Caderno:*", value: question.numero, disabled: !isEditing,},
+        {id: "area", type: "option", label: "Área do Conhecimento:*", options: AreaEnem, value: question.enemArea, disabled: !isEditing,},
+        {id: "materia", type: "option", label: "Disciplina:*", options: materias, value: question.materia._id, disabled: !isEditing,},
+        {id: "frente1", type: "option", label: "Frente Principal:*", options: frentes, value: question.frente1._id, disabled: !isEditing,},
+        {id: "frente2", type: "option", label: "Frente Secundária", options: frentes, value: question.frente2 ? question.frente2._id : '', disabled: !isEditing,},
+        {id: "frente3", type: "option", label: "Frente Terciária", options: frentes, value: question.frente3 ? question.frente3._id : '', disabled: !isEditing,},
     ]
 
     const listFieldInfoQuestion : FormFieldInput[]= [
-        {id: "textoQuestao", type: "textarea", label: "Texto da questão:*", value: newState.textoQuestao, disabled: !isEditing,},
-        {id: "textoAlternativaA", type: "text", label: "Alternativa A:", value: newState.textoAlternativaA, disabled: !isEditing,},
-        {id: "textoAlternativaB", type: "text", label: "Alternativa B:", value: newState.textoAlternativaB, disabled: !isEditing,},
-        {id: "textoAlternativaC", type: "text", label: "Alternativa C:", value: newState.textoAlternativaC, disabled: !isEditing,},
-        {id: "textoAlternativaD", type: "text", label: "Alternativa D:", value: newState.textoAlternativaD, disabled: !isEditing,},
-        {id: "textoAlternativaE", type: "text", label: "Alternativa E:", value: newState.textoAlternativaE, disabled: !isEditing,}, 
+        {id: "textoQuestao", type: "textarea", label: "Texto da questão:*", value: question.textoQuestao, disabled: !isEditing,},
+        {id: "textoAlternativaA", type: "text", label: "Alternativa A:", value: question.textoAlternativaA, disabled: !isEditing,},
+        {id: "textoAlternativaB", type: "text", label: "Alternativa B:", value: question.textoAlternativaB, disabled: !isEditing,},
+        {id: "textoAlternativaC", type: "text", label: "Alternativa C:", value: question.textoAlternativaC, disabled: !isEditing,},
+        {id: "textoAlternativaD", type: "text", label: "Alternativa D:", value: question.textoAlternativaD, disabled: !isEditing,},
+        {id: "textoAlternativaE", type: "text", label: "Alternativa E:", value: question.textoAlternativaE, disabled: !isEditing,}, 
     ]
 
     
 
     const QuestionImageModal = () => {
         if(!photoOpen) return null
-        return <ModalImage handleClose={() => setPhotoOpen(false) }  image={`https://api.vcnafacul.com.br/images/${newState.imageId}.png`} />
+        return <ModalImage handleClose={() => setPhotoOpen(false) }  image={`https://api.vcnafacul.com.br/images/${question.imageId}.png`} />
     }
 
     const handleSave = (data: any) => {
@@ -88,7 +87,7 @@ function ModalDetalhes({ question, infos, handleClose, handleUpdateQuestionStatu
         <>
             <form onSubmit={handleSubmit(handleSave)} className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-7 gap-x-4">
                 <div className="col-span-2 flex flex-col">
-                    <Text className="flex w-full justify-center gap-4 items-center" size="tertiary">Informação do Cursinho {getStatusIcon(newState.status)}</Text>
+                    <Text className="flex w-full justify-center gap-4 items-center" size="tertiary">Informação do Cursinho {getStatusIcon(question.status)}</Text>
                     <Form className="grid grid-cols-1 gap-y-1 mb-1" formFields={listFieldClassification} register={register} />
                 </div>
                 <div className="col-span-3">
@@ -97,7 +96,7 @@ function ModalDetalhes({ question, infos, handleClose, handleUpdateQuestionStatu
                 </div>
                 <div className="col-span-2">
                     <Text className="flex w-full justify-center gap-4 items-center" size="tertiary">Imagem da Questão</Text>
-                    <img className="max-h-96 bg-lightGray p-[1px] w-full mr-4 sm:m-0 cursor-pointer" src={`https://api.vcnafacul.com.br/images/${newState.imageId}.png`} onClick={() => setPhotoOpen(true)} />
+                    <img className="max-h-96 bg-lightGray p-[1px] w-full mr-4 sm:m-0 cursor-pointer" src={`https://api.vcnafacul.com.br/images/${question.imageId}.png`} onClick={() => setPhotoOpen(true)} />
                     <div className="grid grid-cols-2 gap-1">
                         {btns.map(btn => {
                             if(isEditing === btn.editing){
