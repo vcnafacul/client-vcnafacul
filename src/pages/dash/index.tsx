cef9d53f828e902a2f1a6e0f2cd1831e7f35a40f
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-01-24 11:51:06 -0300

    refactor: define imagem default dash main - sugestão mockup

--- src/pages/dash/index.tsx
+++ src/pages/dash/index.tsx
@@ -1,8 +1,17 @@
+import { ReactComponent as HeroSvg } from '../../assets/images/dashboard/dashboard-hero.svg'
+import Text from '../../components/atoms/text'
+
 function Dash(){
     return (
             <div className="w-full flex justify-center">
-                <div className="w-96">
-                    Dash
+                <div className="w-full h-screen flex justify-center items-center">
+                    <div className='flex flex-col md:flex-row items-center'>
+                        <HeroSvg className='min-w-[500px] w-[30vw] max-w-5xl'/>
+                        <div className='w-96'>
+                            <Text className='md:text-left'> Bem Vindos ao Você na Facul</Text>
+                            <Text className='md:text-left' size='tertiary'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</Text>
+                        </div>
+                    </div>
                 </div>
             </div>
     )
