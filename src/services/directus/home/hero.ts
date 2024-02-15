01a4408e910175e8907a1b67c8787e51ac029cb5
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-02-15 09:58:52 -0300

    🥅 refactor: ajusta mensagem de errado ao buscar conteúdo Hero

--- src/services/directus/home/hero.ts
+++ src/services/directus/home/hero.ts
@@ -2,9 +2,13 @@ import { Slide } from "../../../components/organisms/hero";
 import { hero } from "../../urls";
 
 export async function getHeroSlides(): Promise<Slide[]> {
-  const response = await fetch(hero, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" }, 
-  })
-  return (await response.json()).data
+  try {
+    const response = await fetch(hero, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" }, 
+    })
+    return (await response.json()).data
+  } catch (error) {
+    throw Error('Não foi possível recuperar conteúdo Hero')
+  }
 }
