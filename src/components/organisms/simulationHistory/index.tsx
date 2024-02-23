92a64534918c20910e3cdc9b6aaffa9d839620c3
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-02-23 10:30:00 -0300

    refactor: ajusta min detalhe mobile layout

--- src/components/organisms/simulationHistory/index.tsx
+++ src/components/organisms/simulationHistory/index.tsx
@@ -4,10 +4,10 @@ import SimulationHistoryCard from "../../molecules/simulationHistoryCard";
 
 function SimulationHistory() {
     return ( 
-        <div className="w-full pr-20 my-10">
+        <div className="w-full px-4 sm:pr-20 my-10">
             <Text size="primary" className="text-start">Histórico de Simulados</Text>
             <Text size="tertiary" className="text-start text-gray-500">Veja aqui todos os simulados que você já realizou.</Text>
-            <div className="relative">
+            <div className="relative ">
                 <div className="w-40 h-40 bg-green3 absolute right-0 -top-20 -z-10 rotate-45" />
                 <div className="border border-green3 bg-white">
                     {HIstoricosMock.map((historico) => <SimulationHistoryCard historico={historico} />)}
