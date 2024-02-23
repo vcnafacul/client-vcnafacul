d5986d06c21b63c34a5aaacff386e1f9adad6830
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-02-23 10:19:20 -0300

    refactor: sepração logica componente historico simulado

---
+++ src/components/organisms/simulationHistory/index.tsx
@@ -0,0 +1,20 @@
+import { HIstoricosMock } from "../../../pages/mainSimulate/mock";
+import Text from "../../atoms/text";
+import SimulationHistoryCard from "../../molecules/simulationHistoryCard";
+
+function SimulationHistory() {
+    return ( 
+        <div className="w-full pr-20 my-10">
+            <Text size="primary" className="text-start">Histórico de Simulados</Text>
+            <Text size="tertiary" className="text-start text-gray-500">Veja aqui todos os simulados que você já realizou.</Text>
+            <div className="relative">
+                <div className="w-40 h-40 bg-green3 absolute right-0 -top-20 -z-10 rotate-45" />
+                <div className="border border-green3 bg-white">
+                    {HIstoricosMock.map((historico) => <SimulationHistoryCard historico={historico} />)}
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default SimulationHistory;
--- src/pages/mainSimulate/index.tsx
+++ src/pages/mainSimulate/index.tsx
@@ -3,14 +3,11 @@ import Text from "../../components/atoms/text"
 import Ul from "../../components/atoms/ul"
 import CardSimulate from "../../components/molecules/cardSimulate"
 import CarouselRef from "../../components/organisms/carouselRef"
+import SimulationHistory from "../../components/organisms/simulationHistory"
 import { ICard } from "../../types/simulado/ISimulateData"
 import { breakpointsBook, breakpointsDay, simulateData } from "./data"
 import NewSimulate from "./modals/newSimulate"
 import './styles.css'
-import { HIstoricosMock } from "./mock"
-import { getFormatingTime } from '../../utils/getFormatingTime'
-import { getStatusIcon } from "../../utils/getStatusIcon"
-import { StatusEnum } from "../../enums/generic/statusEnum"
 
 function MainSimulate() {
     const [initialize, setInitialize]= useState<boolean>(false);
@@ -43,56 +40,25 @@ function MainSimulate() {
 
     return (
         <>
-                <div className="relative pb-1">
-                    <div className="relative sm:mx-10">
-                        <div className="flex flex-col items-start pt-10 mb-20">
-                            <Text size="primary" className="mb-1">{simulateData.titleBook}</Text>
-                            <Text size="tertiary" className="text-xl">{simulateData.subTitleBook}</Text>
-                            <CarouselRef className="w-full" childrens={cardsBook} breakpoints={breakpointsBook} />
+            <div className="relative pb-1">
+                <div className="relative sm:mx-10">
+                    <div className="flex flex-col items-start pt-10 mb-20">
+                        <Text size="primary" className="mb-1">{simulateData.titleBook}</Text>
+                        <Text size="tertiary" className="text-xl">{simulateData.subTitleBook}</Text>
+                        <CarouselRef className="w-full" childrens={cardsBook} breakpoints={breakpointsBook} />
+                    </div>
+                    <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-10 gap-8">
+                        <div className="flex flex-col items-start sm:col-span-1 md:col-span-3">
+                            <Text size="primary">{simulateData.titleDay}</Text>
+                            <Text size="tertiary" className="text-start text-xl mt-8">{simulateData.subTitleDay}</Text>
                         </div>
-                        <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-10 gap-8">
-                            <div className="flex flex-col items-start sm:col-span-1 md:col-span-3">
-                                <Text size="primary">{simulateData.titleDay}</Text>
-                                <Text size="tertiary" className="text-start text-xl mt-8">{simulateData.subTitleDay}</Text>
-                            </div>
-                            <div className="sm:col-span-2 md:col-span-7 flex justify-center">
-                            <CarouselRef className="w-[448px] md:w-full" childrens={cardsDay} breakpoints={breakpointsDay} />
-                            </div>
-                        </div>
-                        <div className="w-full pr-20 my-10">
-                            <Text size="primary" className="text-start">Histórico de Simulados</Text>
-                            <Text size="tertiary" className="text-start text-gray-500">Veja aqui todos os simulados que você já realizou.</Text>
-                            <div className="relative">
-                                <div className="w-40 h-40 bg-green3 absolute right-0 -top-20 -z-10 rotate-45" />
-                                <div className="border border-green3 bg-white">
-                                    {HIstoricosMock.map((item) => {
-                                        return (
-                                            <div className="flex flex-wrap gap-4 bg-white p-4 m-2.5 shadow-md justify-between">
-                                                        <div className="flex min-w-[200px]">
-                                                            <div>Caderno:  </div>
-                                                            <div className="font-bold">{item.simulado.tipo.nome}</div>
-                                                        </div>
-                                                        <div className="flex gap-2 min-w-[120px]">
-                                                            <div className="font-bold">{item.simulado.tipo.quantidadeTotalQuestao} </div>
-                                                            <div>questões</div>
-                                                        </div>
-                                                        <div className="flex min-w-[120px]">
-                                                            <div>tempo:</div>
-                                                            <div className="font-bold">{getFormatingTime(item.tempoRealizado)}</div>
-                                                        </div>
-                                                        <div className="flex min-w-[200px]">
-                                                            <div>Aproveitamento:</div>
-                                                            <div className="font-bold">{(item.aproveitamento.geral * 100).toFixed(2)} %</div>
-                                                        </div>
-                                                        <div>{getStatusIcon(item.questoesRespondidas === item.simulado.tipo.quantidadeTotalQuestao ? StatusEnum.Approved : StatusEnum.Rejected)}</div>
-                                                    </div>
-                                            )
-                                        })}
-                                </div>
-                            </div>
+                        <div className="sm:col-span-2 md:col-span-7 flex justify-center">
+                        <CarouselRef className="w-[448px] md:w-full" childrens={cardsDay} breakpoints={breakpointsDay} />
                         </div>
                     </div>
+                    <SimulationHistory />
                 </div>
+            </div>
             <ModalNewSimulate />
         </>
     )
