37b9c4205ea8ed4718514178d6de9e268a47fcdc
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-02-23 10:27:27 -0300

    refactor: ajusta tamanho mínimo campo field historico

--- src/components/molecules/simulationHistoryCard/index.tsx
+++ src/components/molecules/simulationHistoryCard/index.tsx
@@ -11,10 +11,10 @@ interface SimulationHistoryCardProps {
 function SimulationHistoryCard({ historico } : SimulationHistoryCardProps) {
     return ( 
         <div className="flex flex-wrap gap-4 bg-white p-4 m-2.5 shadow-md justify-between">
-            <SimulationHistoryField field="Caderno:" value={historico.simulado.tipo.nome} className="min-w-[200px]" />
+            <SimulationHistoryField field="Caderno:" value={historico.simulado.tipo.nome} className="min-w-[250px]" />
             <SimulationHistoryField value="questões" field={historico.simulado.tipo.quantidadeTotalQuestao.toString()} className="min-w-[120px]" />
-            <SimulationHistoryField field="Tempo:" value={getFormatingTime(historico.tempoRealizado)} className="min-w-[120px]" />
-            <SimulationHistoryField field="Aproveitamento:" value={`${(historico.aproveitamento.geral * 100).toFixed(2)}%`} />
+            <SimulationHistoryField field="Tempo:" value={getFormatingTime(historico.tempoRealizado)} className="min-w-[160px]" />
+            <SimulationHistoryField field="Aproveitamento:" value={`${(historico.aproveitamento.geral * 100).toFixed(2)}%`} className="min-w-[200px]" />
             <div>{getStatusIcon(historico.questoesRespondidas === historico.simulado.tipo.quantidadeTotalQuestao ? StatusEnum.Approved : StatusEnum.Rejected)}</div>
         </div>
      );
