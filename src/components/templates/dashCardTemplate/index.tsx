c4418b931778bc18f8fdf8d59c467a00fd95cbaa
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-02-26 18:06:48 -0300

    ♻️ refactor: refatora DashCardTemplate para definir referencia de primeiro e ultimo card quando necessario

--- src/components/templates/dashCardTemplate/index.tsx
+++ src/components/templates/dashCardTemplate/index.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 import Text from "../../atoms/text";
-import CardDash, { CardDashInfo } from "../../molecules/cardDash";
+import { CardDash, CardDashInfo } from "../../molecules/cardDash";
 
 interface DashCardTemplateProps {
     title: string;
@@ -7,9 +9,27 @@ interface DashCardTemplateProps {
     cardlist: CardDashInfo[];
     className?: string;
     onClickCard: (id: number | string) => void;
+    onLoadMoreCard?: (isDown: boolean) => void;
+    limitCardPerPage?: number;
 }
 
-function DashCardTemplate({ title, filterList, cardlist, onClickCard}: DashCardTemplateProps) {
+function DashCardTemplate({ title, filterList, cardlist, onClickCard, onLoadMoreCard, limitCardPerPage = 50}: DashCardTemplateProps) {
+
+    const [ firstCardRef, firstCardInView ] = useInView()
+    const [lastCardRef, lastCardInView] = useInView();
+
+    useEffect(() => {
+        if (onLoadMoreCard && (
+            (lastCardInView && cardlist.length > 0) || 
+            (firstCardInView && cardlist.length === limitCardPerPage)
+        )) {
+            onLoadMoreCard(lastCardInView)
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const gapBeforeLast = 10
+    const indexLastCardInView = cardlist.length - gapBeforeLast
     return (
         <div className="w-full flex justify-center flex-col py-4 ">
             <div className="w-full flex items-center flex-col mt-4">
@@ -21,9 +41,20 @@ function DashCardTemplate({ title, filterList, cardlist, onClickCard}: DashCardT
                 </div>
             </div>
             <div className="flex flex-wrap gap-4 my-4 justify-center md:justify-start md:mx-10">
-                {cardlist.map(card => (
-                    <CardDash onClick={() => onClickCard(card.cardId)} key={card.cardId} title={card.title} infos={card.infos} status={card.status} />
-                ))}
+
+                {cardlist.map((card, index) => {
+                    let ref = null
+                    if(cardlist.length >= limitCardPerPage) {
+                        if(index === indexLastCardInView) ref = lastCardRef
+                        else {
+                            if(limitCardPerPage * 4 === cardlist.length && index === (cardlist.length - limitCardPerPage)) ref = firstCardRef
+                        }
+                    }
+                    if(ref) console.log(ref, index)
+                    return (
+                        <CardDash ref={ref} key={card.cardId} id={`${card.cardId}`} onClick={() => onClickCard(card.cardId)} title={card.title} infos={card.infos} status={card.status}  />
+                    )
+                })}
             </div>
         </div>
     )
