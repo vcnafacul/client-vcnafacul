0cba59ce64a3c3857fd27782f91f8f3d87a66977
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-02-26 18:07:17 -0300

    ♻️refactor: add forwardRef para definir ref externa ao card

--- src/components/molecules/cardDash/index.tsx
+++ src/components/molecules/cardDash/index.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps } from "react"
+import { ComponentProps, forwardRef } from "react"
 import { getStatusIcon } from "../../../utils/getStatusIcon";
 import { StatusContent } from "../../../enums/content/statusContent";
 import { StatusEnum } from "../../../enums/generic/statusEnum";
@@ -19,10 +19,9 @@ export interface CardDash {
 
 export type CardDashProps = CardDash & ComponentProps<'div'>;
 
-function CardDash({ title, status, infos = [], ...props }: CardDashProps) {
-
+export const CardDash = forwardRef<HTMLDivElement, CardDashProps >(({ title, status, infos = [], ...props } : CardDashProps, ref ) => {
     return (
-        <div className="relative w-72 min-h-40 truncate bg-white drop-shadow-xl shadow-xl p-2 cursor-pointer flex flex-col rounded-md pb-8 hover:-translate-y-1 duration-300" {...props}>
+        <div ref={ref} className="relative w-72 min-h-40 truncate bg-white drop-shadow-xl shadow-xl p-2 cursor-pointer flex flex-col rounded-md pb-8 hover:-translate-y-1 duration-300" {...props}>
             <span className="font-bold w-full text-center mb-2">{title}</span>
             {infos.map((info, index) => (
                 <div key={index} className="flex gap-2">
@@ -35,6 +34,4 @@ function CardDash({ title, status, infos = [], ...props }: CardDashProps) {
             </div>
         </div>
     )
-}
-
-export default CardDash
+})
