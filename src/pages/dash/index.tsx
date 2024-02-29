2789b6f2844060b818e905b832eed668a92ddd85
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2024-02-29 12:55:47 -0300

    :bug: fix: adicinoa scrolls independentes para as páginas e para o menu lateral

--- src/pages/dash/index.tsx
+++ src/pages/dash/index.tsx
@@ -1,21 +1,21 @@
-import { ReactComponent as HeroSvg } from '../../assets/images/dashboard/dashboard-hero.svg'
-import Text from '../../components/atoms/text'
-import { Subtitle, Welcome } from './data'
+import { ReactComponent as TriangleGreen } from "../../assets/icons/triangle-green.svg";
+import { ReactComponent as HeroSvg } from '../../assets/images/dashboard/dashboard-hero.svg';
+import Text from '../../components/atoms/text';
+import { Subtitle, Welcome } from './data';
 
-function Dash(){
-    return (
-            <div className="w-full flex justify-center">
-                <div className="w-full h-screen flex justify-center items-center">
-                    <div className='flex flex-col md:flex-row items-center'>
-                        <HeroSvg className='min-w-[500px] w-[30vw] max-w-5xl'/>
-                        <div className='w-96'>
-                            <Text className='md:text-left'>{Welcome}</Text>
-                            <Text className='md:text-left' size='tertiary'>{Subtitle}</Text>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    )
+function Dash() {
+  return (
+     <div className=" relative min-h-[calc(100vh-76px)] flex items-end justify-center">
+        <TriangleGreen className="rotate-180 absolute top-0 left-80 w-40" />
+        <div className='flex flex-col gap-4 items-center justify-center md:flex-row'>
+          <HeroSvg className='min-w-[500px] w-[30vw] max-w-5xl mt-24' />
+          <div className='w-96'>
+            <Text className='md:text-left'>{Welcome}</Text>
+            <Text className='md:text-left' size='tertiary'>{Subtitle}</Text>
+          </div>
+        </div>
+     </div>
+  )
 }
 
 export default Dash
