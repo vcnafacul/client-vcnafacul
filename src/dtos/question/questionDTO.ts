607d092cc1df9f995853c8ccc99abc4c39283513
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-04-15 19:00:07 -0300

    refactor: ajuste para que frente1 seja idenficada pela materia.

--- src/dtos/question/questionDTO.ts
+++ src/dtos/question/questionDTO.ts
@@ -1,35 +1,39 @@
+import { DateTime } from "luxon";
 import { StatusEnum } from "../../enums/generic/statusEnum";
-import { DateTime } from "luxon"
 
 export interface ObjDefault {
-    _id: string;
-    nome: string;
+  _id: string;
+  nome: string;
+}
+
+export interface FrenteObjDefault extends ObjDefault {
+  materia: string;
 }
 
 export interface QuestionDto {
-    _id: string
-    prova: string
-    enemArea: string
-    frente1: string
-    frente2: string
-    frente3: string
-    materia: string
-    numero: number
-    textoQuestao: string
-    textoAlternativaA: string
-    textoAlternativaB: string
-    textoAlternativaC: string
-    textoAlternativaD: string
-    textoAlternativaE: string
-    alternativa: string
-    imageId: string
-    acertos: number
-    quantidadeSimulado: number
-    quantidadeResposta: number
-    status: StatusEnum
-    updatedAt?: DateTime
+  _id: string;
+  prova: string;
+  enemArea: string;
+  frente1: string;
+  frente2: string;
+  frente3: string;
+  materia: string;
+  numero: number;
+  textoQuestao: string;
+  textoAlternativaA: string;
+  textoAlternativaB: string;
+  textoAlternativaC: string;
+  textoAlternativaD: string;
+  textoAlternativaE: string;
+  alternativa: string;
+  imageId: string;
+  acertos: number;
+  quantidadeSimulado: number;
+  quantidadeResposta: number;
+  status: StatusEnum;
+  updatedAt?: DateTime;
 }
 
 export interface Question extends QuestionDto {
-    title: string;
+  title: string;
 }
