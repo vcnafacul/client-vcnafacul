1eb212ab1b48ede4151bae091938291fc7b3e8e9
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-04-21 11:49:14 -0300

    refactor: ajuste layout tela meu perfil

--- src/services/auth/me.ts
+++ src/services/auth/me.ts
@@ -3,14 +3,17 @@ import fetchWrapper from "../../utils/fetchWrapper";
 import { user } from "../urls";
 
 export async function me(token: string): Promise<Auth> {
-    const response = await fetchWrapper(`${user}/me`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` }
-    });
-    const res = await response.json()
-    if(response.status !== 200){
-        throw new Error(`Erro ao buscar Infos Usuário`)
-    }
-    
-    return res
+  const response = await fetchWrapper(`${user}/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const res = await response.json();
+  if (response.status !== 200) {
+    throw new Error(`Erro ao buscar Infos Usuário`);
+  }
+
+  return res;
 }
