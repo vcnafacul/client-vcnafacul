ec313b3b279ba10d10388ab7017c497daefa6ce7
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-04-25 17:05:42 -0300

    feat: implementação validação cadastro por email

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <div className="font-raleway">
       <App />
     </div>
-  </React.StrictMode>,
+  // </React.StrictMode>
 )
