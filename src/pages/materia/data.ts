b83ebbcb8df5c75e3b64fd37743213b470f0b74a
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-04-29 22:24:43 -0300

    refactor: refatora materias de feature conteúdo

--- src/pages/materia/data.ts
+++ src/pages/materia/data.ts
@@ -1,95 +1,72 @@
-import { ReactComponent as LPT } from '../../assets/icons/materias/LPT.svg'
-import { ReactComponent as Grammar } from '../../assets/icons/materias/Grammar.svg'
-import { ReactComponent as Literature } from '../../assets/icons/materias/Literature.svg'
-import { ReactComponent as English } from '../../assets/icons/materias/English.svg'
-import { ReactComponent as Languages } from '../../assets/icons/materias/Languages.svg'
-import { ReactComponent as Biology } from '../../assets/icons/materias/Biology.svg'
-import { ReactComponent as Astronaut } from '../../assets/icons/materias/Astronaut.svg'
-import { ReactComponent as Science } from '../../assets/icons/materias/Science.svg'
-import { ReactComponent as Math } from '../../assets/icons/materias/Math.svg'
-import { ReactComponent as History } from '../../assets/icons/materias/History.svg'
-import { ReactComponent as Geo } from '../../assets/icons/materias/Geo.svg'
-import { ReactComponent as Thoughts } from '../../assets/icons/materias/Thoughts.svg'
-import { ReactComponent as TakingEarth } from '../../assets/icons/materias/TakingEarth.svg'
-import { Materias } from '../../enums/content/materias'
+import { ReactComponent as Astronaut } from "../../assets/icons/materias/Astronaut.svg";
+import { ReactComponent as Biology } from "../../assets/icons/materias/Biology.svg";
+import { ReactComponent as Geo } from "../../assets/icons/materias/Geo.svg";
+import { ReactComponent as Grammar } from "../../assets/icons/materias/Grammar.svg";
+import { ReactComponent as History } from "../../assets/icons/materias/History.svg";
+import { ReactComponent as LPT } from "../../assets/icons/materias/LPT.svg";
+import { ReactComponent as Literature } from "../../assets/icons/materias/Literature.svg";
+import { ReactComponent as Math } from "../../assets/icons/materias/Math.svg";
+import { ReactComponent as Science } from "../../assets/icons/materias/Science.svg";
+import { ReactComponent as TakingEarth } from "../../assets/icons/materias/TakingEarth.svg";
+import { ReactComponent as Thoughts } from "../../assets/icons/materias/Thoughts.svg";
+import { Materias } from "../../enums/content/materias";
 
 export interface MateriaPage {
-    title: string
-    image: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
-    id: Materias
+  image: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  id: Materias;
 }
 
 export interface DataMateriaProps {
-    LPT: MateriaPage
+    LinguaPortuguesa: MateriaPage;
 }
 
 export const dataMateria = {
-    LPT : {
-        title: 'da Leitura e Produção de Texto',
-        image: LPT,
-        id: Materias.LPT
-    },
-    Gramatica : {
-        title: 'da Gramática',
-        image: Grammar,
-        id: Materias.Gramatica
-    },
-    Literatura : {
-        title: 'da Literatura',
-        image: Literature,
-        id: Materias.Literatura
-    },
-    Ingles : {
-        title: 'do Inglês',
-        image: English,
-        id: Materias.Ingles
-    },
-    Espanhol : {
-        title: 'do Espanhol',
-        image: Languages,
-        id: Materias.Espanhol
-    },
-    Biologia : {
-        title: 'da Biologia',
-        image: Biology,
-        id: Materias.Biologia
-    },
-    Fisica : {
-        title: 'da Física',
-        image: Astronaut,
-        id: Materias.Fisica
-    },
-    Quimica : {
-        title: 'da Química',
-        image: Science,
-        id: Materias.Quimica
-    },
-    Matematica : {
-        title: 'da Matematica',
-        image: Math,
-        id: Materias.Matematica
-    },
-    Historia : {
-        title: 'da História',
-        image: History,
-        id: Materias.Historia
-    }
-    ,
-    Geografia : {
-        title: 'da Geografia',
-        image: Geo,
-        id: Materias.Geografia
-    }
-    ,
-    Filosofia : {
-        title: 'da Filosofia',
-        image: Thoughts,
-        id: Materias.Filosofia
-    }
-    ,
-    Sociologia : {
-        title: 'da Sociologia',
-        image: TakingEarth,
-        id: Materias.Sociologia
-    }
-} as DataMateriaProps
+  LinguaPortuguesa: {
+    image: LPT,
+    id: Materias.LinguaPortuguesa,
+  },
+  LinguaEstrangeira: {
+    image: Grammar,
+    id: Materias.LinguaEstrangeira,
+  },
+  Artes: {
+    image: Literature,
+    id: Materias.Artes,
+  },
+  Biologia: {
+    image: Biology,
+    id: Materias.Biologia,
+  },
+  Fisica: {
+    image: Astronaut,
+    id: Materias.Fisica,
+  },
+  Quimica: {
+    image: Science,
+    id: Materias.Quimica,
+  },
+  Matematica: {
+    image: Math,
+    id: Materias.Matematica,
+  },
+  Historia: {
+    image: History,
+    id: Materias.Historia,
+  },
+  Geografia: {
+    image: Geo,
+    id: Materias.Geografia,
+  },
+  Filosofia: {
+    image: Thoughts,
+    id: Materias.Filosofia,
+  },
+  Sociologia: {
+    image: TakingEarth,
+    id: Materias.Sociologia,
+  },
+  Atualidades: {
+    image: TakingEarth,
+    id: Materias.Atualidades,
+  },
+} as DataMateriaProps;
--- src/pages/materia/index.tsx
+++ src/pages/materia/index.tsx
@@ -1,93 +1,110 @@
-import Text from '../../components/atoms/text'
+import { useCallback, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { DataMateriaProps, dataMateria } from './data';
-import { useCallback, useEffect, useState } from 'react';
-import SimpleCardColor from '../../components/atoms/simpleCardColor';
-import { useAuthStore } from '../../store/auth';
-import { getFrentesWithContent } from '../../services/content/getFrentes';
-import { toast } from 'react-toastify';
-import { CONTENT } from '../../routes/path';
-import { Frente } from '../../types/content/frenteContent';
+import { toast } from "react-toastify";
+import SimpleCardColor from "../../components/atoms/simpleCardColor";
+import Text from "../../components/atoms/text";
+import { CONTENT } from "../../routes/path";
+import { getFrentesWithContent } from "../../services/content/getFrentes";
+import { useAuthStore } from "../../store/auth";
+import { Frente } from "../../types/content/frenteContent";
+import { MateriasLabel } from "../../types/content/materiasLabel";
+import { DataMateriaProps, dataMateria } from "./data";
 
-function Materia(){
-    const { nomeMateria } = useParams();
-    const { data: { token } } = useAuthStore();
-    const navigate = useNavigate()
+function Materia() {
+  const { nomeMateria } = useParams();
+  const {
+    data: { token },
+  } = useAuthStore();
+  const navigate = useNavigate();
 
-    const [frentes, setFrentes] = useState<Frente[]>([])
-    const [frenteSelected, setFrenteSelected] = useState<Frente>()
+  const [frentes, setFrentes] = useState<Frente[]>([]);
+  const [frenteSelected, setFrenteSelected] = useState<Frente>();
 
-    const materiaPageInfo = dataMateria[nomeMateria as keyof DataMateriaProps]
-    const Icon = materiaPageInfo.image
+  const materiaPageInfo = dataMateria[nomeMateria as keyof DataMateriaProps];
+  const Icon = materiaPageInfo.image;
 
-    const Frentes = useCallback(() => {
-        if(frentes.length === 0) return null
-        return (
-            <>
-                <Text className='w-fit'>O que estudar?</Text>
-                <div className='flex gap-8 pt-4 pb-8 select-none flex-wrap'>
-                    {frentes.map(frente => <SimpleCardColor 
-                        onClick={() => setFrenteSelected(frente)}
-                        selected={frente.id !== frenteSelected!.id} 
-                        name={frente.name} 
-                        key={frente.id} />)}
-                </div>
-                <div className='w-full border'></div>
-            </>
-        )
-    },[frenteSelected, frentes])
-
-    const Temas = useCallback(() => {
-        if(!frenteSelected) return null
-        return (
-            <div className='pt-4 pb-8'>
-                <Text size='secondary' className='w-fit'>E qual tema?</Text>
-                <div className='flex flex-wrap gap-4'>
-                    {frenteSelected.subject.map(subject => 
-                        <SimpleCardColor name={subject.name}  key={subject.id} 
-                        onClick={() => {
-                            navigate(`/dashboard/${CONTENT}/${subject.name.replace(' ', '')}/${subject.id}`)
-                        }}/>
-                    )}
-                </div>
-            </div>
-        )
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [frenteSelected])
-
-    useEffect(() => {
-        getFrentesWithContent(materiaPageInfo.id, token)
-            .then(res => {
-                setFrentes(res)
-                if(res.length > 0){
-                    setFrenteSelected(res[0])
-                }
-                else {
-                    setFrenteSelected(undefined)
-                }
-            })
-            .catch((error: Error) => {
-                toast.error(error.message)
-            });
-    }, [materiaPageInfo.id, nomeMateria, token])
-
+  const Frentes = useCallback(() => {
+    if (frentes.length === 0) return null;
     return (
-        <div className='py-4 container mx-auto px-4'>
-            <div className='flex items-center justify-between flex-col-reverse md:flex-row'>
-                <div className=''>
-                    <Text className=' text-start'>Bem vindo ao mundo {materiaPageInfo.title}</Text>
-                    <Text size='tertiary' className='text-start m-0 font-semibold'>
-                        Veja dicas de como estudar {materiaPageInfo.title} <a className='text-blue-400' href="#">clicando aqui</a>
-                    </Text>
-                </div>
-                <Icon className='w-[500px] h-[400px]' />
-            </div>
-            <div>
-                <Frentes />
-                <Temas />
-            </div>
+      <>
+        <Text className="w-fit">O que estudar?</Text>
+        <div className="flex gap-8 pt-4 pb-8 select-none flex-wrap">
+          {frentes.map((frente) => (
+            <SimpleCardColor
+              onClick={() => setFrenteSelected(frente)}
+              selected={frente.id !== frenteSelected!.id}
+              name={frente.name}
+              key={frente.id}
+            />
+          ))}
         </div>
-    )
+        <div className="w-full border"></div>
+      </>
+    );
+  }, [frenteSelected, frentes]);
+
+  const Temas = useCallback(() => {
+    if (!frenteSelected) return null;
+    return (
+      <div className="pt-4 pb-8">
+        <Text size="secondary" className="w-fit">
+          E qual tema?
+        </Text>
+        <div className="flex flex-wrap gap-4">
+          {frenteSelected.subject.map((subject) => (
+            <SimpleCardColor
+              name={subject.name}
+              key={subject.id}
+              onClick={() => {
+                navigate(
+                  `/dashboard/${CONTENT}/${subject.name.replace(" ", "")}/${
+                    subject.id
+                  }`
+                );
+              }}
+            />
+          ))}
+        </div>
+      </div>
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [frenteSelected]);
+
+  useEffect(() => {
+    getFrentesWithContent(materiaPageInfo.id, token)
+      .then((res) => {
+        setFrentes(res);
+        if (res.length > 0) {
+          setFrenteSelected(res[0]);
+        } else {
+          setFrenteSelected(undefined);
+        }
+      })
+      .catch((error: Error) => {
+        toast.error(error.message);
+      });
+  }, [materiaPageInfo.id, nomeMateria, token]);
+
+  return (
+    <div className="py-4 container mx-auto px-4">
+      <div className="flex items-center justify-between flex-col-reverse md:flex-row">
+        <div className="">
+          <Text className=" text-start">
+            {`Bem vindo ao mundo da 
+            ${
+              MateriasLabel.find((e) => e.value === materiaPageInfo.id)
+                ?.label || ""
+            }`}
+          </Text>
+        </div>
+        <Icon className="w-[500px] h-[400px]" />
+      </div>
+      <div>
+        <Frentes />
+        <Temas />
+      </div>
+    </div>
+  );
 }
 
-export default Materia
+export default Materia;
