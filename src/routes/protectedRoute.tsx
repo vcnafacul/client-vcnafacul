76162b1339fad199b53ddfba3ce7ebfb490cecde
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-05-11 16:34:45 -0300

    refactor: remove dados de authenticação caso o token esteja expirado

--- src/routes/protectedRoute.tsx
+++ src/routes/protectedRoute.tsx
@@ -1,20 +1,28 @@
+import { DateTime } from "luxon";
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { HOME_PATH } from "./path";
 import { useAuthStore } from "../store/auth";
+import { jwtDecoded } from "../utils/jwt";
+import { HOME_PATH } from "./path";
 
 interface ProtectedRouteProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-function ProtectedRoute({ children } : ProtectedRouteProps) {
+function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const {
+    data: { token },
+    logout,
+  } = useAuthStore();
 
-  const { data: { token }} = useAuthStore()
-
-    if (!token) {
-      return <Navigate to={HOME_PATH} replace />;
-    }
-  
-    return children;
+  if (!token) {
+    return <Navigate to={HOME_PATH} replace />;
   }
-export default ProtectedRoute
+  const decoded = jwtDecoded(token);
+  if (new Date(decoded.exp * 1000) < DateTime.now().toJSDate()) {
+    logout();
+    return <Navigate to={HOME_PATH} replace />;
+  }
+  return children;
+}
+export default ProtectedRoute;
