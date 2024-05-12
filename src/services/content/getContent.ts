c06412fa4073b0bb5b236ef4a59dbf98304e2c48
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-05-12 16:18:40 -0300

    refactor: ajusta apresentação demand

--- src/services/content/getContent.ts
+++ src/services/content/getContent.ts
@@ -1,34 +1,59 @@
-
-import { ContentDtoInput, ContentDtoInputOrder } from "../../dtos/content/contentDtoInput";
+import {
+  ContentDtoInput,
+  ContentDtoInputOrder,
+} from "../../dtos/content/contentDtoInput";
 import { StatusContent } from "../../enums/content/statusContent";
 import fetchWrapper from "../../utils/fetchWrapper";
 import { Paginate } from "../../utils/paginate";
 import { content } from "../urls";
 
-export async function getContent (token: string, status: StatusContent, subjectId?: number, 
-    page: number = 1, limit: number = 40): Promise<Paginate<ContentDtoInput>> {
-    const subject = subjectId ? `&subjectId=${subjectId}` : ''
-    const response = await fetchWrapper(`${content}?status=${status}${subject}&page=${page}&limit=${limit}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` }
-    });
-    const res = await response.json()
-    if(response.status !== 200){
-        throw new Error(`Erro ao buscar Conteúdos Cadastradas ${res.message}`)
+export async function getContent(
+  token: string,
+  status: StatusContent,
+  materia: number,
+  page: number = 1,
+  limit: number = 40
+): Promise<Paginate<ContentDtoInput>> {
+  const subject = materia > -1 ? `&materia=${materia}` : "";
+  const response = await fetchWrapper(
+    `${content}?status=${status}${subject}&page=${page}&limit=${limit}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }
-    return res
+  );
+  const res = await response.json();
+  if (response.status !== 200) {
+    throw new Error(`Erro ao buscar Conteúdos Cadastradas ${res.message}`);
+  }
+  return res;
 }
 
-export async function getContentOrder (token: string, status?: StatusContent, subjectId?: number): Promise<ContentDtoInputOrder[]> {
-    const subject = subjectId ? `&subjectId=${subjectId}` : ''
-    const statusQuery = status ? `status=${status}` : ''
-    const response = await fetchWrapper(`${content}/order?${statusQuery}${subject}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` }
-    });
-    const res = await response.json()
-    if(response.status !== 200){
-        throw new Error(`Erro ao buscar Conteúdos Cadastradas em Ordem ${res.message}`)
+export async function getContentOrder(
+  token: string,
+  status?: StatusContent,
+  subjectId?: number
+): Promise<ContentDtoInputOrder[]> {
+  const subject = subjectId ? `&subjectId=${subjectId}` : "";
+  const statusQuery = status ? `status=${status}` : "";
+  const response = await fetchWrapper(
+    `${content}/order?${statusQuery}${subject}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }
-    return res
+  );
+  const res = await response.json();
+  if (response.status !== 200) {
+    throw new Error(
+      `Erro ao buscar Conteúdos Cadastradas em Ordem ${res.message}`
+    );
+  }
+  return res;
 }
