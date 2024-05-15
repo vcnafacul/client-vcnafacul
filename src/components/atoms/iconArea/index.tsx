1afd429be30bb8b6ab0b9873880a6670cb8fbc5a
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-05-15 19:30:56 -0300

    fix: corrige icone oval simulado

--- src/components/atoms/iconArea/index.tsx
+++ src/components/atoms/iconArea/index.tsx
@@ -6,8 +6,8 @@ interface IconAreaProps {
 function IconArea({ icon, className } : IconAreaProps) {
     const Icone = icon;
     return (
-        <div className={`w-16 h-16 rounded-full flex justify-center items-center ${className}`}>
-            <Icone className="m-2 p-1" />
+        <div className={`w-16 rounded-full flex justify-center items-center ${className}`}>
+            <Icone className="m-2 p-1 w-12 h-12 fill-white" />
         </div>
     )
 }
