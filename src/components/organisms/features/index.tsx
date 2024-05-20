aa5d9319a89c9d68173dea08e8d81d20ec28c8eb
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-05-20 18:13:50 -0300

    fix: add class relative no className que recebe o Selector em Features

--- src/components/organisms/features/index.tsx
+++ src/components/organisms/features/index.tsx
@@ -4,45 +4,63 @@ import Text from "../../atoms/text";
 import Selector from "../../molecules/selector";
 import { ItemCard } from "../actionAreas";
 import FeatureSkeleton from "../featuresSkeleton";
-import './styles.css';
+import "./styles.css";
 
 interface FeaturesItemsProps {
-    Home_Features_Item_id: ItemCard
+  Home_Features_Item_id: ItemCard;
 }
 
 export interface FeaturesProps {
-    title: string;
-    subtitle: string;
-    items: FeaturesItemsProps[]
+  title: string;
+  subtitle: string;
+  items: FeaturesItemsProps[];
 }
 
 function Features() {
-    const { features  } = useHomeContext()
-    const [feature, setFeature] = useState<ItemCard | undefined>(features?.items[0].Home_Features_Item_id);
+  const { features } = useHomeContext();
+  const [feature, setFeature] = useState<ItemCard | undefined>(
+    features?.items[0].Home_Features_Item_id
+  );
 
-    const changeItem = (index : number) => {
-        setFeature(features!.items[index].Home_Features_Item_id)
+  const changeItem = (index: number) => {
+    setFeature(features!.items[index].Home_Features_Item_id);
+  };
+
+  useEffect(() => {
+    if (features) {
+      setFeature(features.items[0].Home_Features_Item_id);
     }
+  }, [features]);
 
-    useEffect(() => {
-        if(features) {
-            setFeature(features.items[0].Home_Features_Item_id)
-        }
-    }, [features])
-
-
-    if(!features) return <FeatureSkeleton />
-    return (
-        <div className=" text-center  overflow-hidden flex items-center flex-col">
-            <Text size="secondary" className="mt-2">{features!.title}</Text>
-            <Text size="tertiary">{features!.subtitle}</Text>
-            <div className="md:grid md:grid-cols-12 md:mx-16 flex w-full m-0 flex-col items-center">
-                <Selector align="vertial" changeItem={changeItem} tabItems={features?.items.map(item => (item.Home_Features_Item_id.title))}/>
-                <img className="object-cover object-center flex-grow-0 w-full max-w-[653px] md:h-auto md:text-center md:my-0 mx-10 transition duration-500 ease-linear" src={feature?.image as string}/>
-                <Text size="tertiary" className="text-left text-grey md:text-center md:max-w-[623px] description md:text-base md:mt-3 mx-10">{feature?.subtitle}</Text>
-            </div>
-        </div>
-    )
+  if (!features) return <FeatureSkeleton />;
+  return (
+    <div className=" text-center  overflow-hidden flex items-center flex-col">
+      <Text size="secondary" className="mt-2">
+        {features!.title}
+      </Text>
+      <Text size="tertiary">{features!.subtitle}</Text>
+      <div className="md:grid md:grid-cols-12 md:mx-16 flex w-full m-0 flex-col items-center relative">
+        <Selector
+          align="vertial"
+          changeItem={changeItem}
+          tabItems={features?.items.map(
+            (item) => item.Home_Features_Item_id.title
+          )}
+        />
+        <img
+          className="object-cover object-center flex-grow-0 w-full max-w-[653px] md:h-auto 
+          md:text-center md:my-0 mx-10 transition duration-500 ease-linear"
+          src={feature?.image as string}
+        />
+        <Text
+          size="tertiary"
+          className="text-left text-grey md:text-center md:max-w-[623px] description md:text-base md:mt-3 mx-10"
+        >
+          {feature?.subtitle}
+        </Text>
+      </div>
+    </div>
+  );
 }
 
-export default Features
+export default Features;
