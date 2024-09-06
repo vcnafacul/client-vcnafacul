68d870e8ff11047baefebd390d926e894b93afcf
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-09-06 15:57:11 -0300

    refactor: ajusta informações data directus

--- src/pages/home/data.ts
+++ src/pages/home/data.ts
@@ -31,9 +31,9 @@ import HeroImg2 from "../../assets/images/home/hero_teacher.svg";
 
 import feature1 from "../../assets/images/home/1-Plataforma personalizada - comp.png";
 import feature2 from "../../assets/images/home/2-Conteudos pre-vestibular - comp.png";
-import feature3 from "../../assets/images/home/3-Redaçoes corrigidas - comp.png";
 import feature4 from "../../assets/images/home/4-Exercicios e Simulados online - comp.png";
 import feature5 from "../../assets/images/home/5-Forum de duvidas - comp.png";
+import feature6 from "../../assets/images/home/6-Exercicios e Simulados online - comp.png";
 
 import { ReactComponent as homeSubjectArte } from "../../assets/icons/home-subjects-arte.svg";
 import { ReactComponent as homeSubjectAtualidades } from "../../assets/icons/home-subjects-atualidades.svg";
@@ -364,10 +364,10 @@ export const features: FeaturesProps = {
     {
       Home_Features_Item_id: {
         id: 1,
-        title: "Plataforma personalizada",
+        title: "Simulados online do ENEM",
         subtitle:
-          "Ao acessar a plataforma Você na Facul você terá um painel só seu, personalizado de acordo com seu perfil e seu progresso nos estudos! [EM BREVE]",
-        image: feature1,
+          "O Você na Facul disponibiliza simulados automáticos com questões reais para você praticar quantas vezes quiser! E o resultado sai na hora! [EM EXPANSÃO]",
+        image: feature6,
       },
     },
     {
@@ -375,26 +375,17 @@ export const features: FeaturesProps = {
         id: 2,
         title: "Conteúdos pré-vestibular",
         subtitle:
-          "Você terá acesso aos melhores conteúdos pré-vestibular que existem hoje, que foram selecionados com carinho e organizados para você! [EM BREVE]",
+          "Você terá acesso aos melhores conteúdos pré-vestibular que existem hoje, que foram selecionados com carinho e organizados para você! [EM CONSTRUÇÃO]",
         image: feature2,
       },
     },
     {
       Home_Features_Item_id: {
         id: 3,
-        title: "Suas redações corrigidas",
+        title: "Plataforma personalizada",
         subtitle:
-          "Tá precisando melhorar nas redações? É só escrever quantas quiser e enviar pelo painel que um professor vai corrigir e dar sugestões de melhoria! [EM BREVE]",
-        image: feature3,
-      },
-    },
-    {
-      Home_Features_Item_id: {
-        id: 4,
-        title: "Exercícios e Simulados online",
-        subtitle:
-          "O Você na Facul disponibiliza simulados automáticos com questões reais para você praticar quantas vezes quiser! E o resultado sai na hora! [EM BREVE]",
-        image: feature4,
+          "Ao acessar a plataforma Você na Facul você terá um painel só seu, personalizado de acordo com seu perfil e seu progresso nos estudos! [EM BREVE]",
+        image: feature1,
       },
     },
     {
@@ -404,6 +395,15 @@ export const features: FeaturesProps = {
         subtitle:
           "Tá com dúvidas sobre os conteúdos? É só mandar uma mensagem no fórum de dúvidas que algum professor vai te ajudar com o maior prazer! [EM BREVE]",
         image: feature5,
+      },
+    },
+    {
+      Home_Features_Item_id: {
+        id: 4,
+        title: "Suas redações corrigidas",
+        subtitle:
+          "OTá precisando melhorar nas redações? É só escrever quantas quiser e enviar pelo painel que um professor vai corrigir e dar sugestões de melhoria! [EM BREVE]",
+        image: feature4,
       },
     },
   ],
