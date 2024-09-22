4910351c7e28f96e642e390acd2cdb877e852baf
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-09-22 19:49:12 -0300

    feat: implementa chamada para completar cadastro

---
+++ src/services/prepCourse/student/inscription.ts
@@ -0,0 +1,24 @@
+import { StudentInscriptionDTO } from "@/dtos/student/studentInscriptionDTO";
+import { studentCourse } from "@/services/urls";
+import fetchWrapper from "@/utils/fetchWrapper";
+
+export async function completeInscriptionStudent(
+  student: StudentInscriptionDTO,
+  token: string
+): Promise<void> {
+  const response = await fetchWrapper(studentCourse, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      ...student,
+      socioeconomic: JSON.stringify(student.socioeconomic),
+    }),
+  });
+  const res = await response.json();
+  if (response.status !== 201) {
+    throw new Error(res.message);
+  }
+}
