fd18a64652b0bdf92c8ef71e8c8a080d73c4c5f3
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-11-30 19:36:33 -0300

    refactor: invester ordem conteudo e carousel

--- src/pages/newsPage/index.tsx
+++ src/pages/newsPage/index.tsx
@@ -1,74 +1,82 @@
-import { useEffect, useState } from "react"
-import HeroTemplate from "../../components/templates/heroTemplate"
-import Text from "../../components/atoms/text"
-import { News } from "../../dtos/news/news"
-import { getNews } from "../../services/news/getNews"
-import Content from "../../components/atoms/content"
-import NewsCarousel from "../../components/organisms/newsCarousel"
-import { toast } from "react-toastify"
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import { ReactComponent as TriangleGreen } from "../../assets/icons/triangle-green.svg";
+import Content from "../../components/atoms/content";
+import Text from "../../components/atoms/text";
+import NewsCarousel from "../../components/organisms/newsCarousel";
+import HeroTemplate from "../../components/templates/heroTemplate";
+import { News } from "../../dtos/news/news";
+import { getNews } from "../../services/news/getNews";
 
-function NewsPage(){
-    const [news, setNews] = useState<News[]>([])
-    const [indexSelect, setIndexSelect] = useState<number>(0);
+function NewsPage() {
+  const [news, setNews] = useState<News[]>([]);
+  const [indexSelect, setIndexSelect] = useState<number>(0);
 
-    const VITE_BASE_FTP = import.meta.env.VITE_BASE_FTP;
+  const VITE_BASE_FTP = import.meta.env.VITE_BASE_FTP;
 
-    const NewContent = () => {
-      if(news.length > 0) {
-        return (
-          <Content docxFilePath={`${VITE_BASE_FTP}${news[indexSelect].fileName}`} />
-        )
-      }
-      return null
+  const NewContent = () => {
+    if (news.length > 0) {
+      return (
+        <Content
+          docxFilePath={`${VITE_BASE_FTP}${news[indexSelect].fileName}`}
+        />
+      );
     }
+    return null;
+  };
 
-    const breakpoints = {
-      1: {
-          slidesPerView: 1,
-      },
-      350: {
-          slidesPerView: 1.5,
-      },
-      388: {
-          slidesPerView: 2,
-      },
-      700: {
-          slidesPerView: 3,
+  const breakpoints = {
+    1: {
+      slidesPerView: 1,
+    },
+    350: {
+      slidesPerView: 1.5,
+    },
+    388: {
+      slidesPerView: 2,
+    },
+    700: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  };
 
-      },
-      1000: {
-          slidesPerView: 4,
-      },
-      1200: {
-          slidesPerView: 5,
-      },
-    }
+  useEffect(() => {
+    getNews()
+      .then((res) => {
+        setNews(res.data);
+      })
+      .catch((error: Error) => {
+        toast.error(error.message);
+      });
+  }, []);
 
-
-    useEffect(() => {
-        getNews()
-          .then(res => {
-            setNews(res.data)
-          })
-          .catch((error: Error) => {
-            toast.error(error.message)
-          })
-      }, [])
-      
-    return (
-        <HeroTemplate headerPosition="fixed">
-            <div className="relative flex items-center flex-col">
-                <div className="bg-white h-12 min-w-[400px] max-w-[500px] relative -top-7 z-50 text-center text-xl text-grey py-2">
-                  {news.length == 0 ? null : news[indexSelect].session}
-                </div>
-                <Text size="secondary" className="text-grey font-normal">{news.length == 0 ? null : news[indexSelect].title}</Text>
-                <TriangleGreen className="absolute w-[500px] -right-[250px] bottom-0" />
-                <NewContent />
-                <NewsCarousel news={news} breakpoints={breakpoints} onClickCard={(index: number) => { setIndexSelect(index)}} />
-            </div>
-        </HeroTemplate>
-    )
+  return (
+    <HeroTemplate headerPosition="fixed">
+      <div className="relative flex items-center flex-col">
+        <NewsCarousel
+          news={news}
+          breakpoints={breakpoints}
+          onClickCard={(index: number) => {
+            setIndexSelect(index);
+          }}
+        />
+        <div className="bg-white h-12 min-w-[400px] max-w-[500px] relative -top-7 z-50 text-center text-xl text-grey py-2">
+          {news.length == 0 ? null : news[indexSelect].session}
+        </div>
+        <Text size="secondary" className="text-grey font-normal">
+          {news.length == 0 ? null : news[indexSelect].title}
+        </Text>
+        <TriangleGreen className="absolute w-[500px] -right-[250px] bottom-0" />
+        <NewContent />
+      </div>
+    </HeroTemplate>
+  );
 }
 
-export default NewsPage
+export default NewsPage;
