2f97aee4af65af5152e6514049592cc315d51916
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2025-01-14 19:31:39 -0300

    remove: removendo strictMode

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,9 @@ import "./index.css";
 import "primereact/resources/themes/md-light-indigo/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <div className="font-raleway">
       <PrimeReactProvider>
         <App />
       </PrimeReactProvider>
     </div>
-  </React.StrictMode>
 );
