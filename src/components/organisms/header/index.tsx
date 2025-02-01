2c7e50be732d6c5b1d69326540a6f56d58b7a511
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2025-01-31 21:01:13 -0300

    refactor: remove botão menu e introduz botão header

--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -2,61 +2,88 @@ import { useState } from "react";
 
 import { ReactComponent as MenuIcon } from "../../../assets/icons/menu.svg";
 
+import { ReactComponent as Reporticon } from "../../../assets/icons/warning.svg";
 import { useBaseTemplateContext } from "../../../context/baseTemplateContext";
 import { useAuthStore } from "../../../store/auth";
 import { capitalize } from "../../../utils/capitalize";
+import { SocialLink } from "../../molecules/followUs";
 import Logged from "../../molecules/Logged";
 import Logo from "../../molecules/logo";
 import { ItemMenuProps } from "../../molecules/menuItems";
 import MainMenu from "../mainMenu";
 import Sign from "../sign";
-import { SocialLink } from "../../molecules/followUs";
+import DropdwonMenu from "@/components/atoms/dropdownMenu";
+import { report } from "@/pages/home/data";
 
 export interface HeaderData {
-    pageLinks: ItemMenuProps[]
-    socialLinks: SocialLink[];
-    userNavigationSign: ItemMenuProps[]
-    userNavigationLogged: ItemMenuProps[]
+  pageLinks: ItemMenuProps[];
+  socialLinks: SocialLink[];
+  userNavigationSign: ItemMenuProps[];
+  userNavigationLogged: ItemMenuProps[];
 }
 
 interface HeaderProps {
-    solid: boolean;
-    className?: string;
+  solid: boolean;
+  className?: string;
 }
 
 function Header({ solid, className }: HeaderProps) {
-    const [openMenu, setOpenMenu] = useState(false);
-    const { data: { token, user: { firstName } } } = useAuthStore()
+  const [openMenu, setOpenMenu] = useState(false);
+  const {
+    data: {
+      token,
+      user: { firstName },
+    },
+  } = useAuthStore();
 
-    const { header } = useBaseTemplateContext()
+  const { header } = useBaseTemplateContext();
 
-    const MenuBugger = () => {
-        if (openMenu) return null
-        return (
-            <div onClick={() => setOpenMenu(true)} className="md:hidden">
-                <MenuIcon className={`${!solid ? 'fill-white' : 'fill-marine'}`} />
-            </div>
-        )
-    }
-
+  const MenuBugger = () => {
+    if (openMenu) return null;
     return (
-        <header className={className} id="header">
-            <div className='md:container mx-auto h-full flex items-center'>
-                <div className="flex w-full justify-between items-center mx-4 md:mx-auto md:max-w-6xl ">
-                    <MenuBugger />
-                    <Logo solid={solid} name />
-                    <MainMenu itemsMenu={header.pageLinks} socialLinks={header.socialLinks} solid={solid} open={openMenu}
-                        handleClose={() => {
-                            setOpenMenu(false)
-                        }} />
-                    {!token ?
-                        <Sign userNavigation={header.userNavigationSign} solid={solid} className="items-center gap-2" /> :
-                        <Logged userNavigation={header.userNavigationLogged} userName={capitalize(firstName)} className={solid ? 'text-marine' : 'text-white'} />
-                    }
-                </div>
-            </div>
-        </header>
-    )
+      <div onClick={() => setOpenMenu(true)} className="md:hidden">
+        <MenuIcon className={`${!solid ? "fill-white" : "fill-marine"}`} />
+      </div>
+    );
+  };
+
+  return (
+    <header className={className} id="header">
+      <div className="md:container mx-auto h-full flex items-center">
+        <div className="flex w-full justify-between items-center mx-4 md:mx-auto md:max-w-6xl">
+          <MenuBugger />
+          <Logo solid={solid} name />
+          <MainMenu
+            itemsMenu={header.pageLinks}
+            socialLinks={header.socialLinks}
+            solid={solid}
+            open={openMenu}
+            handleClose={() => {
+              setOpenMenu(false);
+            }}
+          />
+          {!token ? (
+            <Sign
+              userNavigation={header.userNavigationSign}
+              solid={solid}
+              className="items-center gap-2"
+            />
+          ) : (
+            <Logged
+              userNavigation={header.userNavigationLogged}
+              userName={capitalize(firstName)}
+              className={solid ? "text-marine" : "text-white"}
+            />
+          )}
+        </div>
+        <DropdwonMenu
+          userNavigation={report}
+        >
+          <Reporticon className="hidden sm:block w-8 h-8" />
+        </DropdwonMenu>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
