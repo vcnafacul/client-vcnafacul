172cda6a81363d3cfa8e50ce403577170e59c06a
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2025-04-12 14:57:44 -0300

    fix: corrige chamada cadastro geolocation

--- src/services/geolocation/createGeolocation.ts
+++ src/services/geolocation/createGeolocation.ts
@@ -1,11 +1,19 @@
 import { CreateGeolocation } from "../../types/geolocation/geolocation";
 import { geolocations } from "../urls";
 
-export async function createGeolocation( data: CreateGeolocation ) {
+export async function createGeolocation(data: CreateGeolocation) {
   const response = await fetch(geolocations, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
-  })
-  return await response.json()
+  });
+
+  if (response.status === 201) {
+    return await response.json();
+  }
+
+  const errorBody = await response.text();
+  throw new Error(
+    `Falha ao criar geolocalização. Status: ${response.status}. Corpo da resposta: ${errorBody}`
+  );
 }
