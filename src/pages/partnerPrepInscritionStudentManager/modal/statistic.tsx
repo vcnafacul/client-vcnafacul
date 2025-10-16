6d2d60adabaf5817da113fb1004a6a40aae0a9c3
Fernando Almeida <fernando.almeida.pinto@gmail.com>
2025-10-15 21:51:34 -0300

    fix: corrige tela estatistica

--- src/pages/partnerPrepInscritionStudentManager/modal/statistic.tsx
+++ src/pages/partnerPrepInscritionStudentManager/modal/statistic.tsx
@@ -26,13 +26,13 @@ export function Statistic({ geral, enrolleds, handleClose }: Props) {
           <TabsContent value="details" className="h-full">
             <ModalContent onClose={handleClose}>
               <></>
-              {/* <RenderCharts data={geral} /> */}
+              <RenderCharts data={geral} />
             </ModalContent>
           </TabsContent>
           <TabsContent value="enrolled" className="h-full">
             <ModalContent onClose={handleClose}>
               <></>
-              {/* <RenderCharts data={enrolleds} /> */}
+              <RenderCharts data={enrolleds} />
             </ModalContent>
           </TabsContent>
         </Tabs>
