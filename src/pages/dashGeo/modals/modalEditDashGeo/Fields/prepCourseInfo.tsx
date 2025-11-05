5b453cf70a9d07bca01f94c4438ba534c361c900
Fernando Almeida <fernando.almeida.pinto@gmail.com>
2025-11-05 13:30:51 -0300

    fix: corrige dropdown estado visualizar geolocation

--- src/pages/dashGeo/modals/modalEditDashGeo/Fields/prepCourseInfo.tsx
+++ src/pages/dashGeo/modals/modalEditDashGeo/Fields/prepCourseInfo.tsx
@@ -126,6 +126,7 @@ export function PrepCourseInfo({ form, setValue, errors, edit, geo }: Props) {
         options={form.state.options}
         disabled={!edit}
         defaultValue={geo.state}
+        value={geo.state}
       />
       <InputFactory
         id={form.phone.label}
