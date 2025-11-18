a276584c3160d605668d474e26f41ef927036eff
Fernando Almeida <fernando.almeida.pinto@gmail.com>
2025-11-18 14:04:02 -0300

    fix: remove refresh token

--- src/utils/decodedUser.ts
+++ src/utils/decodedUser.ts
@@ -1,25 +1,27 @@
 import { AuthProps } from "../store/auth";
 import { jwtDecoded } from "./jwt";
 
-
-export function decoderUser(access_token: string){
-    const decoded = jwtDecoded(access_token)
-    const birthday = new Date(decoded.user.birthday.replace("Z", ""));
-    const monthBirthday = birthday.getMonth() + 1 < 10 ? `0${birthday.getMonth() + 1}` : birthday.getMonth();
-    const dayBirthday = birthday.getDate() < 10 ? `0${birthday.getDate()}` : birthday.getDate();
-    const payload : AuthProps = {
-        token: access_token,
-        refresh_token: decoded.refresh_token,
-        user: { 
-            ...decoded.user, 
-            birthday: `${dayBirthday}/${monthBirthday}/${birthday.getFullYear()}`,
-            state: decodeURIComponent(decoded.user.state)
-            },
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        permissao: decoded.roles.reduce((obj: any, item: number) => {
-                obj[item] = true;
-                return obj;
-            }, {})
-    };
-    return payload
+export function decoderUser(access_token: string) {
+  const decoded = jwtDecoded(access_token);
+  const birthday = new Date(decoded.user.birthday.replace("Z", ""));
+  const monthBirthday =
+    birthday.getMonth() + 1 < 10
+      ? `0${birthday.getMonth() + 1}`
+      : birthday.getMonth();
+  const dayBirthday =
+    birthday.getDate() < 10 ? `0${birthday.getDate()}` : birthday.getDate();
+  const payload: AuthProps = {
+    token: access_token,
+    user: {
+      ...decoded.user,
+      birthday: `${dayBirthday}/${monthBirthday}/${birthday.getFullYear()}`,
+      state: decodeURIComponent(decoded.user.state),
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    permissao: decoded.roles.reduce((obj: any, item: number) => {
+      obj[item] = true;
+      return obj;
+    }, {}),
+  };
+  return payload;
 }
