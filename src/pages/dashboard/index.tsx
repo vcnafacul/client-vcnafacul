145b62c89c07db5ad433b70774b13e8a8dfc65c8
Fernando Almeida <fernando.almeida.pinto@gmail.com>
2026-04-02 20:04:21 -0300

    refactor: altera cor dash

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,7 +3,7 @@ import { widgetRegistry } from './widgetRegistry';
 
 export default function Dashboard() {
   return (
-    <div className="bg-slate-100/60">
+    <div className="bg-slate-200/50 h-full">
       <div className="w-full mx-auto px-16 pt-8">
         <WidgetGrid widgets={widgetRegistry} />
       </div>
