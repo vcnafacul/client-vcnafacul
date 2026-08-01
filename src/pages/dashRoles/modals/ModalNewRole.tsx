1c917c28ee8ea7222083501ad0fefbc18390fa42
Fernando Almeida <fernando.almeida.pinto@gmail.com>
2026-08-01 18:35:48 -0300

    feat(roles): CreateRoleDto + EMPTY_ROLE com permissões provas cursinho

--- src/pages/dashRoles/modals/ModalNewRole.tsx
+++ src/pages/dashRoles/modals/ModalNewRole.tsx
@@ -54,6 +54,8 @@ const EMPTY_ROLE: CreateRoleDto = {
   supportAgent: false,
   partnerPrepSupportAgent: false,
   editarMateriasFrentes: false,
+  visualizarProvasCursinho: false,
+  cadastrarProvasCursinho: false,
 };
 
 function applyBaseRoleConstraints(
